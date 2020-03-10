3b42e65158afdb127e7507b5c433be2f594f0807
Mustafa <Mustafa.Mohamd@hotmail.com>
2020-03-10 13:37:55 +0300

    hotfix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 2700;
+const port = process.env.PORT || 3000;
 const mongo = require("mongodb").MongoClient;
 const assert = require("assert");
 var cors = require("cors");
